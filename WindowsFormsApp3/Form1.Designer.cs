cfd324052215caf4117f5102da6cf3b8094efc3d
example <email@example.com>
2021-01-08 23:17:13 +0300

    Add project files.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.Color.LightGray;
+            this.button1.Location = new System.Drawing.Point(211, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(277, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BAŞLA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.UseWaitCursor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tw Cen MT Condensed", 20.25F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(147, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "programı başlatın ve hatalarınızı görün..";
+            this.label1.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(578, 210);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Location = new System.Drawing.Point(100, 200);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KONTROL PROGRAMIM";
+            this.TransparencyKey = System.Drawing.Color.WhiteSmoke;
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private TextBox vertical;
+        private string errorText;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SetFourDifferentScrollBars()
+        {
+
+            this.vertical = new System.Windows.Forms.TextBox();
+           
+           
+            string startString = errorText;
+
+            
+            vertical.Location = new Point(10, 70);
+            vertical.ScrollBars = ScrollBars.Vertical;
+            vertical.Multiline = true;
+            vertical.Text = startString + ScrollBars.Vertical.ToString();
+
+           
+            this.Controls.Add(this.vertical);
+           
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+
+            Document document = new Document();
+            document.LoadFromFile(filePath);
+
+            Section section = document.Sections[0];
+
+            Paragraph p1 = section.Paragraphs[0];
+            Paragraph p2 = section.Paragraphs[1];
+            Paragraph pOnsoz = section.Paragraphs[25];
+            
+
+            int pKaynakcaNumber = 0;
+            
+
+                string errorText = "";
+           
+
+                for (int i = section.Paragraphs.Count - 1; i >= 0; i--)
+            {
+                if (section.Paragraphs[i].Text.Trim() == "KAYNAKÇA")
+                {
+                    pKaynakcaNumber = i;
+                }
+       
+            }
+
+         
+
+            Paragraph pKaynakca = section.Paragraphs[pKaynakcaNumber];
+
+         
+
+            string fontType = "Times New Roman";
+            string FontSize = "16";
+            string fontType2 = "Calibri";
+            string FontSize2 = "18";
+            
+
+
+
+
+
+
+
+
+
+            if (p1.BreakCharacterFormat.FontName != fontType)
+            {
+                errorText += "* Üniversite adı yazı tipi hatalıdır ve " + p1.BreakCharacterFormat.FontName + " dir. " + fontType + " olmalıdır.";
+            }
+            if (p1.BreakCharacterFormat.FontSize != 16)
+            {
+                errorText += "\n* Üniversite adı yazı boyutu hatalıdır ve " + p1.BreakCharacterFormat.FontSize + " dir. " + FontSize + " olmalıdır.";
+            }
+           
+
+
+
+
+
+            if (p2.BreakCharacterFormat.FontName != fontType2)
+            {
+                errorText += "\n* yazı tipi hatalıdır ve " + p2.BreakCharacterFormat.FontName + " dir. " + fontType2 + " olmalıdır.";
+            }
+            if (p2.BreakCharacterFormat.FontSize != 16)
+            {
+                errorText += "\n*  yazı boyutu hatalıdır ve " + p2.BreakCharacterFormat.FontSize + " dir. " + FontSize2 + " olmalıdır.";
+            }
+
+
+
+            if (pOnsoz.BreakCharacterFormat.FontName != fontType2)
+            {
+                errorText += "\n* Önsöz yazı tipi hatalıdır ve " + pOnsoz.BreakCharacterFormat.FontName + " dir. " + fontType2 + " olmalıdır.";
+            }
+            if (pOnsoz.BreakCharacterFormat.FontSize != 14)
+            {
+                errorText += "\n* Önsöz yazı boyutu hatalıdır ve " + pOnsoz.BreakCharacterFormat.FontSize + " dir. " + "14" + " olmalıdır.";
+            }
+
+
+
+
+            if (pKaynakca.BreakCharacterFormat.FontName != fontType)
+            {
+                errorText += "\n* Kaynakça yazı tipi hatalıdır ve " + pKaynakca.BreakCharacterFormat.FontName + " dir. " + fontType + " olmalıdır.";
+            }
+            if (pKaynakca.BreakCharacterFormat.FontSize != 18)
+            {
+                errorText += "\n* Kaynakça yazı boyutu hatalıdır ve " + pKaynakca.BreakCharacterFormat.FontSize + " dir. " + "18" + " olmalıdır.";
+            }
+
+
+
+
+                for (int i = pKaynakcaNumber + 1; i <= section.Paragraphs.Count - 1; i++)
+                {
+                if (section.Paragraphs[i].Text.Trim() != "")
+                {
+                    if (section.Paragraphs[i].Text.Substring(0, 1) == "[")
+                    {
+                        if (section.Paragraphs[i].Text.IndexOf(",") == -1)
+                        {
+                            errorText += "\n* Şu kaynakça hatalıdır. Kaynakça tipi hatalıdır. İsim veya adres belirtmede hatalar var -> " + section.Paragraphs[i].Text;
+                        }
+                    }
+                    else
+                    {
+                        errorText += "\n* Şu kaynakça hatalıdır. Başında parantez  yok -> " + section.Paragraphs[i].Text;
+                    }
+                }
+            }
+
+            for (int i = 50; i < pKaynakcaNumber; i++)
+            {
+                if (section.Paragraphs[i].Text.Trim() != "")
+                {
+                    if (section.Paragraphs[i].BreakCharacterFormat.FontName != fontType2)
+                    {
+                        errorText += "\n* İçerikte yazı tipi hatası var. " + section.Paragraphs[i].BreakCharacterFormat.FontName + " yazı tipini -> " + fontType2 + " olarak değiştirin";
+                    }
+                    if (section.Paragraphs[i].BreakCharacterFormat.FontSize != 12)
+                    {
+                        errorText += "\n* İçerikte yazı boyutu hatası var. " + section.Paragraphs[i].BreakCharacterFormat.FontSize + " yazı boyutu -> " + "12" + " olarak değiştirin";
+                    }
+
+                }
+            }
+            errorText += "\n\n**** LÜTFEN BELİRTİLEN HATALARINIZI DÜZELTİNİZ...**** ";
+            MessageBox.Show(errorText, "!!! Hata Raporu");
+
+            // MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
